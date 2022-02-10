25b50b6e8eb9f11e53d3ed4089f989b852168a8e
SaraAditi <rssaranya.09@gmail.com>
2022-02-10 11:42:50 +0530

    Commit message

---
+++ src/test/java/MavenTutorial/AppiumTest.java
@@ -0,0 +1,15 @@
+package MavenTutorial;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void MobileBrowserAutomation() {
+		System.out.println("MobileBrowserAutomation");
+	}
+	@Test
+	public void MobileBrowserInvoke() {
+		System.out.println("MobileBrowserInvoke");
+	}
+
+}
---
+++ src/test/java/MavenTutorial/RestApiTest.java
@@ -0,0 +1,17 @@
+package MavenTutorial;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	@Test
+	public void RestApiAutomation() {
+		System.out.println("RestApiAutomation");
+	}
+	
+	@Test
+	public void RestApiInvoke() {
+		System.out.println("RestApiInvoke");
+	}
+
+}
