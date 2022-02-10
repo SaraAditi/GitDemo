a88b3f322b6ae23f73cd1d609b91af5df0738f83
SaraAditi <rssaranya.09@gmail.com>
2022-02-10 12:47:51 +0530

    changes from 2nd guy

--- src/test/java/MavenTutorial/SeleniumTest.java
+++ src/test/java/MavenTutorial/SeleniumTest.java
@@ -12,6 +12,8 @@ public class SeleniumTest {
 	@Test
 	public void Browserinvoke() {
 		System.out.println("Browserinvoke");
+		System.out.println("GitDemoPractise");
+		System.out.println("cloning in github");
 
 	}
 
